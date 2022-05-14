1308c69ce6546deef4ff815864877a5ff1dec13a
john12458 <john210628@gmail.com>
2022-05-14 14:08:15 +0800

    add recharts

---
+++ fontend/my-app/src/Keyword.js
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+import { LineChart, Line,CartesianGrid, XAxis,YAxis,Tooltip,Legend} from 'recharts';
+//  yyyy-mm-dd
+const TEST_DATA = [
+    { value:"TSMC",count:50,time: "2022-06-30"},
+    { value:"TSMC",count:190,time: "2022-07-01"},
+    { value:"TSMC",count:120,time: "2022-07-02"},
+    { value:"ASML",count:10,time: "2022-06-30"},
+    { value:"ASML",count:60,time: "2022-07-01"},
+    { value:"ASML",count:70,time: "2022-07-02"},
+    { value:"Intel",count:20,time: "2022-06-30"},
+    { value:"Intel",count:30,time: "2022-07-01"},
+    { value:"Intel",count:40,time: "2022-07-02"},
+]
+function getRandomColor() {
+    var letters = '3456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return color;
+  }
+  
+export default function Keyword(params) {
+    const [popData, setpopData] = useState([]);
+    const [data, setData] = useState([])
+    const [lineData, setLineData] = useState([]) 
+    const [startTime, setStartTime] = useState()
+    const [endTime, setEndTime] = useState()
+    const [queryTime, setQueryTime] = useState()
+    const popClose = ()=>setpopData([])
+    useEffect(()=>{
+        setStartTime("2022-06-30")
+        setEndTime("2022-07-02")
+        setQueryTime("2022-07-01")
+    },[])
+
+    useEffect(()=>{
+        if(queryTime != null)
+            setData(TEST_DATA)
+    },[queryTime])
+
+    useEffect(()=>{
+        const dataToLineData = (data)=>{
+            const timeset = new Set(data.map(value => value.time))
+            let lineData = []
+            for (let etime of timeset) {
+                let obj = {time:etime}
+                for ( let el of data){
+                    const {value, count, time} = el
+                    if(time === etime)
+                        obj[value] = count
+                }
+                lineData.push(obj)
+                
+            }
+            
+            return lineData    
+        }
+        setLineData(dataToLineData(data))
+    },[data])
+    useEffect(()=>{
+        if(popData.length !== 0){
+            let interval = setInterval(popClose, 1000);
+            return ()=>clearInterval(interval)
+        }
+    },[popData])
+
+    return (
+        <div>
+            <div style={{justifyContent:"center",paddingTop:10,marginBottom:20}}>
+                <div><span>KEYWORD</span></div>
+                <div><span>{`${startTime} - ${endTime}`}</span></div>
+
+                <LineChart width={1000} height={350} data={ lineData }
+                    margin={{ top: 10 , right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="time" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    {
+                        (lineData.length!== 0)
+                        && Object.keys(Object.values(lineData)[0])
+                            .filter(kname=> kname!== 'time' && kname !== '')
+                            .map((kname,idx)=><Line name={kname} key={kname} type="monotone" dataKey={kname} stroke={getRandomColor()} />)
+                            
+                    }
+                    
+                </LineChart>
+            </div>
+        </div>
+    )
+}
