40615e2c39c6ee7afa7a431877e2331ffd242825
john12458 <john210628@gmail.com>
2022-05-18 15:06:34 +0800

    [TEST OK] TIME FORMAT

--- fontend/my-app/src/pages/test/Keyword.test.jsx
+++ fontend/my-app/src/pages/test/Keyword.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen  } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Keyword from '../Keyword';
-
+import moment from 'moment';
 describe('Keyword Page', () => {
   
   beforeEach(() =>{ // if use beforeall it will auto cleanup in aftereach
@@ -10,8 +11,23 @@ describe('Keyword Page', () => {
   test('Title', () => {
     expect(screen.getByText(/Keyword/i)).toBeInTheDocument();
   });
+  test('Time Format', () => {
+    const timeText = screen.getByText(/\d{4}-\d{2}-\d{2} - \d{4}-\d{2}-\d{2}/i)
+    const [time1,time2] = timeText.textContent.split(' - ')
+
+    expect(moment(time1).isValid()).toBeTruthy()
+    expect(moment(time2).isValid()).toBeTruthy()
+    expect(timeText).toBeInTheDocument();
+  });
 
   test('Recharts Exists',()=>{
+    expect(screen.getByRole('linechart')).toBeTruthy();
+  })
+
+  test('Line Charts Hover Event',()=>{
+    const target = screen.getByRole('linechart')
+    userEvent.hover(target);
+
     expect(screen.getByRole('linechart')).toBeTruthy();
   })
 
