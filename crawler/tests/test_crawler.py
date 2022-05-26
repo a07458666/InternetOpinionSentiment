954a264c2843eef934a6ca63ab20e14b84370214
ChaShaoAn <kevinchao840713@gmail.com>
2022-05-26 23:05:27 +0800

    [modify] add crawler test for word count

--- crawler/tests/test_crawler.py
+++ crawler/tests/test_crawler.py
@@ -41,3 +41,51 @@ def test_html_getText():
     soup = crawler.html_parser(response.text)
     orignal_text = crawler.html_getText(soup)
     assert len(orignal_text) > 0
+
+
+def test_word_count_en():
+    crawler = crawler_sample.GoogleCrawler()
+    target_url = 'https://www.reuters.com/technology/exclusive-ukraine-halts-half-worlds-neon-output-chips-clouding-outlook-2022-03-11/'
+    response = crawler.get_source(target_url)
+    soup = crawler.html_parser(response.text)
+    orignal_text = crawler.html_getText(soup)
+    result_wordcount = crawler.word_count(orignal_text)
+    whitelist = ['tsmc']
+    end_result = crawler.get_wordcount_json(whitelist , result_wordcount)
+    assert end_result[0]['Count'] > 0
+
+
+def test_word_count_en_with_applied_materials():
+    crawler = crawler_sample.GoogleCrawler()
+    target_url = 'https://www.globenewswire.com/news-release/2022/05/26/2451160/0/en/Applied-Materials-New-Ioniq-PVD-System-Solves-Wiring-Resistance-Challenges-of-2D-Scaling.html'
+    response = crawler.get_source(target_url)
+    soup = crawler.html_parser(response.text)
+    orignal_text = crawler.html_getText(soup)
+    result_wordcount = crawler.word_count(orignal_text)
+    whitelist = ['applied materials']
+    end_result = crawler.get_wordcount_json(whitelist , result_wordcount)
+    assert end_result[0]['Count'] > 0
+
+
+def test_word_count_ch():
+    crawler = crawler_sample.GoogleCrawler()
+    target_url = 'https://www.techbang.com/posts/95836-applied-materials-introduces-3d-gaa-transistor-technology'
+    response = crawler.get_source(target_url)
+    soup = crawler.html_parser(response.text)
+    orignal_text = crawler.html_getText(soup)
+    result_wordcount = crawler.word_count(orignal_text)
+    whitelist = ['應用材料']
+    end_result = crawler.get_wordcount_json(whitelist , result_wordcount)
+    assert end_result[0]['Count'] > 0
+
+
+def test_word_count_ch_with_applied_materials():
+    crawler = crawler_sample.GoogleCrawler()
+    target_url = 'https://www.techbang.com/posts/95836-applied-materials-introduces-3d-gaa-transistor-technology'
+    response = crawler.get_source(target_url)
+    soup = crawler.html_parser(response.text)
+    orignal_text = crawler.html_getText(soup)
+    result_wordcount = crawler.word_count(orignal_text)
+    whitelist = ['applied materials']
+    end_result = crawler.get_wordcount_json(whitelist , result_wordcount)
+    assert end_result[0]['Count'] > 0
