6ffc3ac524f63c7ea54e333e77c5330c51a5c007
john12458 <john210628@gmail.com>
2022-06-01 19:52:44 +0800

    add build.sh for fontend build to Pageserver

--- PageServer/app.js
+++ PageServer/app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Prometheus setup
 const register = prom_client.register;
---
+++ build.sh
@@ -0,0 +1,4 @@
+cd ./fontend/my-app
+npm run build
+cd ../../
+cp -r ./fontend/my-app/build ./PageServer 
